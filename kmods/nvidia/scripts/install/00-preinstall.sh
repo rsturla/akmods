4c669ba835a75b0c37e2834f200169ca8920fcb7
Robert Sturla <robertsturla@outlook.com>
2025-04-06 18:19:59 +0100

    fix: remove rpmfusion

--- kmods/nvidia/scripts/install/00-preinstall.sh
+++ kmods/nvidia/scripts/install/00-preinstall.sh
@@ -6,7 +6,5 @@ source /tmp/akmods/info/nvidia-vars
 # Create a backup of current repos
 cp -a /etc/yum.repos.d /tmp/yum.repos.d
 
-# Configure rpmfusion repos
+# Configure repos
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-archive}.repo
-sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}{,-updates}.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
