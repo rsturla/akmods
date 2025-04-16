6fcc149bb36c825260d0b59f20d6f649de819474
Robert Sturla <robertsturla@outlook.com>
2025-04-17 00:24:58 +0100

    chore: use ublue RPM rename script

--- _scripts/final.sh
+++ _scripts/final.sh
@@ -8,21 +8,16 @@ for rpm in $(find /var/cache/rpms -name '*.rpm'); do
   cp -a $rpm /rpms
 done
 
-kernel_version=$(rpm -q --qf "%{VERSION}-%{RELEASE}.%{ARCH}\n" kernel-core | head -n 1)
-
-sed -i -e 's/args = \["rpmbuild", "-bb"\]/args = \["rpmbuild", "-bb", "--buildroot", "#{build_path}\/BUILD"\]/g' /usr/local/share/gems/gems/fpm-*/lib/fpm/package/rpm.rb;
-
+sed -i -e 's/args = \["rpmbuild", "-bb"\]/args = \["rpmbuild", "-bb", "--buildroot", "#{build_path}\/BUILD"\]/g' /usr/local/share/gems/gems/fpm-*/lib/fpm/package/rpm.rb
+kernel_version=$(rpm -q --qf "%{VERSION}-%{RELEASE}.%{ARCH}\n" "${KERNEL_NAME}" | head -n 1)
 for rpm in $(find /rpms -type f -name \*.rpm); do
-  basename="$(basename ${rpm})"
-  # Remove kernel version from the RPM name (e.g. kmod-nvidia-6.11.3-300.fc41.x86_64-3:560.35.03-2.fc41.x86_64 -> kmod-nvidia-3:560.35.03-2.fc41.x86_64)
-  name=$(echo ${basename} | sed -r "s/(-${kernel_version})//g")
-
-  if [[ "$basename" == *"$kernel_version"* ]]; then
-    echo "Processing $rpm with kernel version $kernel_version..."
-    fpm --verbose -s rpm -t rpm -p ${rpm} -f --name ${name} ${rpm};
-  else
-    echo "Skipping $rpm as it does not contain kernel version $kernel_version..."
-  fi
+    basename=$(basename ${rpm})
+    name=${basename%%-${kernel_version}*}
+    if [[ "$basename" == *"$kernel_version"* ]]; then
+        fpm --verbose -s rpm -t rpm -p ${rpm} -f --name ${name} ${rpm}
+    else
+        echo "Skipping $basename rebuild as the name does not contain $kernel_version"
+    fi
 done
 
 ls -l /rpms
