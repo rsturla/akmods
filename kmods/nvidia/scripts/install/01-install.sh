664937fa201c6c048e4f5d4dd3cfab426ef2946b
Robert Sturla <robertsturla@outlook.com>
2025-08-24 02:20:15 +0100

    chore: compare only nvidia versions

--- kmods/nvidia/scripts/install/01-install.sh
+++ kmods/nvidia/scripts/install/01-install.sh
@@ -58,8 +58,8 @@ fi
 dnf install -y "${COMMON_PKGS[@]}" "${ARCH_PKGS[@]}"
 
 # Ensure the version of the Nvidia module matches the driver
-KMOD_VERSION="$(rpm -q --queryformat '%{VERSION}-%{RELEASE}' kmod-nvidia)"
-DRIVER_VERSION="$(rpm -q --queryformat '%{VERSION}-%{RELEASE}' nvidia-driver)"
+KMOD_VERSION="$(rpm -q --queryformat '%{VERSION}' kmod-nvidia)"
+DRIVER_VERSION="$(rpm -q --queryformat '%{VERSION}' nvidia-driver)"
 if [ "$KMOD_VERSION" != "$DRIVER_VERSION" ]; then
     echo "Error: kmod-nvidia version ($KMOD_VERSION) does not match nvidia-driver version ($DRIVER_VERSION)"
     exit 1
